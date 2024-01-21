9907d1d99e084d09463c8ca89e940494fe979c50
Grzegorz Piejko <gp23.1982@gmail.com>
2024-01-21 10:12:00 +0100

    Comments removed

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 import { Wrapper } from "../components/common/wrapper";
 import { Header } from "../components/header";
 import { Skills } from "../components/skills";
@@ -8,18 +7,10 @@ import { Footer } from "../components/footer";
 import { actualSkills, futureSkills } from "../components/skills/skills";
 import actualSkillsIcon from "../assets/images/actualSkillsIcon.png";
 import futureSkillsIcon from "../assets/images/futureSkillsIcon.png";
-import { theme } from "../assets/styles/themes/theme";
 import { useModeContext } from "../assets/styles/ModeContext";
 
 function App() {
   const { mode, toggleMode } = useModeContext();
-
-  // const [mode, setMode] = useState("light");
-
-  // const handleToggleMode = () => {
-  //   console.log("działa bardzo dobrze");
-  //   setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-  // };
 
   return (
     <Wrapper mode={mode}>
