14644ed99a7ac3460f53f341604efa9f6ad9bd85
Grzegorz Piejko <gp23.1982@gmail.com>
2024-02-04 09:16:06 +0100

    Adding links for Icons and buttons

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -20,9 +20,31 @@ export const Footer = () => {
         <StyledInfo />
       </Content>
       <Socials>
-        <StyledGithubIcon />
-        <StyledFacebookIcon />
-        <StyledLinkedInIcon />
+        <a
+          href="https://github.com/gp231982"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="https://github.com/gp231982"
+        >
+          <StyledGithubIcon />
+        </a>
+
+        <a
+          href="https://www.facebook.com/grzegorz.piejko"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="https://www.facebook.com/grzegorz.piejko"
+        >
+          <StyledFacebookIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/grzegorz-piejko-process-development-engineer/"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="https://www.linkedin.com/in/grzegorz-piejko-process-development-engineer/"
+        >
+          <StyledLinkedInIcon />
+        </a>
         <StyledInstagramIcon />
       </Socials>
     </Wrapper>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -15,8 +15,7 @@ import { StyledCaption } from "../common/styled";
 import myPicture from "../../assets/images/myPicture.jpg";
 import { ToggleModeButton } from "./ToggleModeButton";
 
-export const Header = ({toggleMode}) => {
-
+export const Header = ({ toggleMode }) => {
   return (
     <StyledHeader>
       <StyledImgContainer>
@@ -26,10 +25,15 @@ export const Header = ({toggleMode}) => {
         <StyledCaption content="This is" />
         <StyledAboutMeHeader />
         <StyledAboutMe />
-        <StyledButton>
-          <ButtonIcon />
-          <StyledButtonText />
-        </StyledButton>
+        <a
+          href="mailto:grzegorz.piejko23@gmail.com"
+          title="grzegorz.piejko23@gmail.com"
+        >
+          <StyledButton>
+            <ButtonIcon />
+            <StyledButtonText />
+          </StyledButton>
+        </a>
         <ToggleModeButton onClick={toggleMode}>
           <StyledTextContainer />
           <StyledIconsContainer />
